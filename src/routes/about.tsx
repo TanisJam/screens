20c5826ccc2626e690777701d054e1faba39980e
Mauricio Romero <mauricionromero@hotmail.com>
2025-01-06 11:47:02 -0300

    add tanstack router

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,17 +1,17 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
-import { useState } from 'react';
+import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
 
-export const Route = createLazyFileRoute('/about')({
+export const Route = createFileRoute('/about')({
   component: About,
-});
+})
 
 function About() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   return (
     <div className="p-2">
       <h3>Welcome to About!</h3>
       <button onClick={() => setCount((c) => c + 1)}>Increment</button>
       <div>Count: {count}</div>
     </div>
-  );
+  )
 }
